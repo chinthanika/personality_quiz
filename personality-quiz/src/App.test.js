cb5f9cca97dc8b9b31173746bc8ae212c5f915c2
Chinthanika Iddamalgoda <85867263+chinthanika@users.noreply.github.com>
2024-04-03 13:10:01 +0800

    Updates

--- personality-quiz/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders a quiz question', () => {
-  render(<App />);
-  // Update this to match a known text element from your App component
-  const questionElement = screen.getByText(/Do you enjoy being the center of attention?/i);
-  expect(questionElement).toBeInTheDocument();
-});
